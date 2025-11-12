8196e9457c2125cb9d61ab4ffb4bd03454e46109
Henrique AC <henriqueac00@gmail.com>
2025-11-11 23:46:26 -0300

    login parcial
    apenas login, add autenticacao e logout

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import LoginView
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- src/home/views.py
+++ src/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
 
 def index(request):
     return render(request, 'index.html')
+
+class LoginView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
