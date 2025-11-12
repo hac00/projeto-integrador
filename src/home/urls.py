024c41dd78a8c5c0cb179fc780cc7c811040c18e
Henrique AC <henriqueac00@gmail.com>
2025-11-12 11:50:32 -0300

    Registro Home

--- src/home/urls.py
+++ src/home/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
 from . import views
-from .views import LoginView
+from .views import LoginView, IndexView
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', IndexView.as_view(), name='home'),
     path('login/', LoginView.as_view(), name='login'),
 ]
--- src/home/views.py
+++ src/home/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView
+
+from pessoa.models import Funcionario, Pessoa
+from veiculo.models import Veiculo
+from estacionamento.models import Vaga, Valor
 
 
-def index(request):
-    return render(request, 'index.html')
+# def index(request):
+#     return render(request, 'index.html')
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self):
+        context = super(IndexView, self).get_context_data()
+        context['qtd_clientes'] = Pessoa.objects.count() - Funcionario.objects.count()
+        context['qtd_funcionarios'] = Funcionario.objects.count()
+        context['qtd_veiculos'] = Veiculo.objects.count()
+        context['qtd_vagas'] = Vaga.objects.count()
+        context['valor'] = Valor.objects.first().valor_hora
+        return context
 
 class LoginView(LoginView):
     template_name = 'login.html'
     redirect_authenticated_user = True
+
+
