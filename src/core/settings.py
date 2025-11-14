e7d4bf5322c083477b4226c1a9cd196ab2091dd5
Henrique AC <henriqueac00@gmail.com>
2025-11-14 01:07:30 -0300

    SETTINGS.PY: remocao de configs para enviar email

--- src/core/settings.py
+++ src/core/settings.py
@@ -133,10 +133,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'henriqueac00@gmail.com'
-EMAIL_HOST_PASSWORD = 'sqvs ciyi dqht eeit'
-DEFAULT_FROM_EMAIL = 'Estacionamento'
